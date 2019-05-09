a593cf6ffbbb831445954ff584666396b8ba542a
joaoteixeirams <49699198+joaoteixeirams@users.noreply.github.com>
2019-05-09 20:19:06 -0300

    alfabetoMilitar.js

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,13 @@
+function alfabetoMilitar(x) {
+    if( x==="a") {return "alpha"}
+    if( x==="A") {return "alpha"}
+    if( x==="e") {return "echo"}
+    if( x==="E") {return "echo"}
+    if( x==="i") {return "india"}
+    if( x==="I") {return "india"}
+    if( x==="o") {return "oscar"}
+    if( x==="O") {return "oscar"}
+    if( x==="u") {return "uniform"}
+    if( x==="U") {return "uniform"}
+ else {return false}
+}
