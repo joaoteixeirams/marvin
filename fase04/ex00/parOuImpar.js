e23df2e105f41e941042d4a3bb7abcebe8fcf80e
joaoteixeirams <49699198+joaoteixeirams@users.noreply.github.com>
2019-05-09 20:15:06 -0300

    parOuImpar.js

--- fase04/ex00/parOuImpar.js
+++ fase04/ex00/parOuImpar.js
@@ -1,6 +1,6 @@
-function parOuImpar(tei) {
-      if( tei % 2 === 0){
-      return "Par";
-   }
-   return "Ímpar";
-}
+function parOuImpar(numero) {
+    if (numero %2===0)
+    return "Par"
+else
+ return "Ímpar"
+} 
