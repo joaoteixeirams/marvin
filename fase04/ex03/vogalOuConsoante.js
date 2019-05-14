a0cac96229cf1383d5aaa9f1393b82b0d553d11a
joaoteixeirams <49699198+joaoteixeirams@users.noreply.github.com>
2019-05-14 19:22:46 -0300

    vogalouconsoanre

--- fase04/ex03/vogalOuConsoante.js
+++ fase04/ex03/vogalOuConsoante.js
@@ -1,7 +1,10 @@
-function vogalOuConsoante(tei) {
-     if( tei === "a" || tei=== "e" ||tei=== "i"|| tei=== "o" ||tei=== "u")
-     {return "Vogal"}
-     else {return "Consoante"}
-
-}   
-     
+function vogalOuConsoante(letra){
+    var resp = "Consoante";
+    letra = letra.toLowerCase();
+    
+    if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
+        resp = "Vogal";
+    }   
+    
+    return resp;
+}
