674e488bc3fa8cf654780b23e74064ba406e7606
joaoteixeirams <49699198+joaoteixeirams@users.noreply.github.com>
2019-05-14 19:23:23 -0300

    abc

--- fase04/ex04/abc.js
+++ fase04/ex04/abc.js
@@ -1,7 +1,11 @@
-function abc(tei) {
-     if( tei === "a" ||tei=== "b" ||tei=== "c")
-     {return true}
-     else {return false}
-
-}   
-     
+function abc(str){
+    str = str.charAt(0);
+    str = str.toLowerCase();
+    
+    var resp = false;
+    
+    if(str == "a" || str == "b" || str == "c"){
+        resp = true;
+    }
+    return resp;
+}
